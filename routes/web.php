fdc32874295baaa70b581a9c40e1780fc37df56a
EnricoClem <enrico.clem94@gmail.com>
2024-06-03 16:13:47 +0200

    ADD - admin route based on dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+});
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
